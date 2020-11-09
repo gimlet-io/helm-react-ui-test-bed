51ae28794da18f536b6d961e98ace21c483ac86c
Laszlo Fogas <laszlo@laszlo.cloud>
2020-11-09 13:44:57 +0100

    Controlling values; ranges

--- src/app.js
+++ src/app.js
@@ -1,21 +1,37 @@
-import React, { Component} from "react";
-import {hot} from "react-hot-loader";
+import React, { Component } from 'react'
+import { hot } from 'react-hot-loader'
 import * as schema from '../fixtures/values.schema.json'
 import * as helmUIConfig from '../fixtures/helm-ui.json'
 import HelmUI from 'helm-react-ui'
-import './style.css';
+import './style.css'
 
-class App extends Component{
-  render(){
-    return(
+class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      values: {}
+    }
+    this.setValues = this.setValues.bind(this);
+  }
+
+  setValues (values) {
+    console.log('setting state')
+    this.setState({ values: values })
+  }
+
+  render () {
+    return (
       <div className="container mx-auto m-8">
         <HelmUI
           schema={schema.default}
           config={helmUIConfig.default}
+          values={this.state.values}
+          setValues={this.setValues}
         />
       </div>
-    );
+    )
   }
 }
 
-export default hot(module)(App);
+export default hot(module)(App)
